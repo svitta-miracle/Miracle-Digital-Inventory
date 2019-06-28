41a4e2339acb4a1033513176497b792dc04b79b9
svitta-miracle <svitta@miraclesoft.com>
2019-06-28 10:38:28 -0400

    third push

--- Inventory/src/main/java/com/mss/Inventory/repository/UserDao.java
+++ Inventory/src/main/java/com/mss/Inventory/repository/UserDao.java
@@ -1,12 +1,11 @@
 package com.mss.Inventory.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mss.Inventory.entity.Users;
 
 @Repository
-public class UserDao extends MongoRepository<Users,Integer> {
-	
-	
+public interface UserDao extends MongoRepository<Users,String> {
 
 }
